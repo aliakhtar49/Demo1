5f48df077904b3260c29eff752a837cfc4ec223e
ali akhtar <aliakhtarcs11020@gmail.com>
2021-03-29 14:55:47 +0500

    Update CoreDummy.swift

--- Home/Home/CoreDummy.swift
+++ Home/Home/CoreDummy.swift
@@ -19,38 +19,3 @@ public class CoreDummy {
     
     }
 }
-
-//final public class GoogleAutocompleteClient {
-//    private let placesClient: GMSPlacesClient
-//    private let token: GMSAutocompleteSessionToken
-//    private let filter: GMSAutocompleteFilter
-//    private let debounceInterval: DispatchTimeInterval
-//
-//    /// Creates an instance with the provided parameters.
-//    ///
-//    /// - Parameters:
-//    ///   - placesClient:       The `GMSPlacesClient`. `.shared()` by default.
-//    ///   - filter:             The `GMSAutocompleteFilter`.
-//    ///   - token:              The `GMSAutocompleteSessionToken`. A new by default.
-//    ///   - debounceInterval:   The `DispatchTimeInterval`. `.milliseconds(500)` by default.
-//    public init(
-//        placesClient: GMSPlacesClient = .shared(),
-//        filter: GMSAutocompleteFilter,
-//        token: GMSAutocompleteSessionToken = GMSAutocompleteSessionToken(),
-//        debounceInterval: DispatchTimeInterval = .milliseconds(500)
-//    ) {
-//        self.placesClient = placesClient
-//        self.filter = filter
-//        self.token = token
-//        self.debounceInterval = debounceInterval
-//    }
-//
-//
-//
-//
-//    private func reduceFields(_ fields: [GMSPlaceField]) -> GMSPlaceField? {
-//        guard !fields.isEmpty else { return nil }
-//        guard fields.count > 1 else { return fields.first }
-//        return GMSPlaceField(rawValue: fields.map { $0.rawValue }.reduce(0, |))
-//    }
-//}
